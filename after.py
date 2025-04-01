3fe941a29577c789ef69499ff9ca6a9412cee0f7
maneetHTML <maneet.harmeet@gmail.com>
2025-04-01 17:16:03 +1300

    hi

---
+++ after.py
@@ -0,0 +1,18 @@
+class dog:
+    taste="pitbull"#taste is a class variable 
+    taste="Amarica pitbull"#taste is a class variable 
+    def __init__(self,name,age):
+        self.nam=name
+        self.ag=age
+    def info(self): #info is an user defined method
+        print("Dogs are cute.")
+obj=dog("jam","2")
+print(obj.nam,obj.taste)
+print(obj.ag, "year old")
+
+
+obj=dog("suff","3")
+print(obj.nam,obj.taste)
+print(obj.ag, "year old")
+
+obj.info()
